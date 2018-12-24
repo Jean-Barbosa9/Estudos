ffbde8a95161f8984b84af23c40cb4c209448cfc
Jean Barbosa <jeanbarbosa9@gmail.com>
2018-12-24 10:11:18 -0200

    criando parte da função que desenhará as bandeiras de tempo em tempo

--- web-development/front-end/Alura/Logica-de-programacao-II--pratique-com-desenhos--animacoes-e-um-jogo/js/drawing-flags.js
+++ web-development/front-end/Alura/Logica-de-programacao-II--pratique-com-desenhos--animacoes-e-um-jogo/js/drawing-flags.js
@@ -6,8 +6,14 @@ class DrawingFlags {
     this.canvas = new DrawCanvas(this.width, this.height,'#ffffff')
     this.interval
     this.flagsList = this.flagsList()
+    this.flagsLen = this.flagsList.length
     this.actualFlag = this.flagsList[0]
     this.nextFlag = this.flagsList[this.flagsList.indexOf(this.actualFlag)+1]
+
+    this.formIndex = 0
+    this.listIndex = 0
+
+
     this.drawSettings()
   }
 
@@ -25,26 +31,64 @@ class DrawingFlags {
     const { maxWidth, maxHeight, settings } = this.flags
     const country = settings.flags.country
     let settingsOf = settings.flags[this.flagsList.indexOf(this.actualFlag)],
-    types = []
+    types = [],
+    formsLen = settingsOf.forms.length
 
-    for(var i=0, len=settingsOf.forms.length;i<len;i++) {
-      types.push(settingsOf.forms[i].type)
-      // aqui precisará pegar as coords, conforme type
-      this.mountForm(types[0])
-    }
+    // for(var i=0, len=settingsOf.forms.length;i<len;i++) {
+    //   types.push(settingsOf.forms[i].type)
+    //   let props = settingsOf.forms[i].coords
+    //   this.mountForm(types[i],props)
+    // }
 
-    console.log('types: ', types[0]);
+
+    // tenho que pensar em uma forma de incrementar o item que está sendo lido pela lista na hora de desenhar a bandeira
+
+
+    this.interval = setInterval(() => {
+
+
+      if(this.formIndex<formsLen) {
+        console.log(this.actualFlag);
+        console.log(this.listIndex);
+        types.push(settingsOf.forms[this.formIndex].type)
+        let props = settingsOf.forms[this.formIndex].coords
+        this.mountForm(types[this.formIndex],props)
+        this.formIndex++
+        console.log('running')
+      }
+      else if(this.listIndex < this.flagsLen) {
+        this.actualFlag = this.flagsList[this.flagsList.indexOf(this.actualFlag)+1]
+        this.listIndex++
+        this.formIndex = 0
+        this.drawSettings()
+      }
+      else {
+        this.stop()
+        this.canvas.clear(0,0,this.width,this.height)
+      }
+    },1000)
+
   }
 
-  mountForm(type) {
-    if(type == 'rect') {
-      this.canvas.rect(0,0,600,400,"#000")
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  mountForm(type, props) {
+    if(type == 'circle') {
+      this.canvas.circle(props.x,props.y,props.ray,props.color)
+    }
+    else if(type == 'rect') {
+      this.canvas.rect(props.initialX,props.initialY,props.width,props.height,props.color)
+    }
+    else if(type == 'polygon' || type == 'line') {
+      this.canvas.polygon(props)
     }
   }
 
   drawFlag() {
+    // aqui talvez poderia vir o setInterval que desenhará a bandeira aos poucos
 
-    this.canvas.rect()
   }
 
   drawBrazilFlag() {
